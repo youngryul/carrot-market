6221baf317fde815cd597c6844365665f3a5a3c3
PNS212624\Hansol <yrlee00@hansol.com>
2024-11-20 17:53:38 +0900

    feat: 핸드폰 화면 구현

---
+++ app/phone/page.tsx
@@ -0,0 +1,11 @@
+export default function Phone() {
+    return (
+        <div className="bg-orange-500 h-screen w-screen flex justify-center items-center">
+            <div className="bg-black h-3/4 w-1/3 rounded-3xl p-8">
+                <div className="bg-white h-full w-full rounded-3xl">
+
+                </div>
+            </div>
+        </div>
+    )
+}
